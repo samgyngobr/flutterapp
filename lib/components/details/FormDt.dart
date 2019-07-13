97893cc3ef7f64a42431ae5d550584edde1f8d32
samgyngobr <samuel0360@gmail.com>
2019-07-13 12:32:52 -0300

    1

--- lib/components/details/FormDt.dart
+++ lib/components/details/FormDt.dart
@@ -12,19 +12,53 @@ class FormDt extends StatefulWidget
 
 class _FormDtState extends State<FormDt> {
 
+
+  selectForm( val )
+  {
+    switch ( val )
+    {
+      case 'One'  : return Container( child: Text( 'One' ) );
+      case 'Two'  : return Container( child: Text( 'Two' ) );
+      case 'Tree' : return Container( child: Text( 'Tree' ) );
+      case 'Four' : return Container( child: Text( 'Four' ) );
+    }
+  }
+
+  String dropdownValue = 'One';
+
   @override
   Widget build(BuildContext context)
   {
     return Container(
         child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: ListTile(
-          title: Text( 'TItulo' ),
-          subtitle: Text( 'subtitulo' ),
-          leading: Icon( Icons.whatshot, color: Colors.red, size: 40.0, ),
-          trailing: Icon( Icons.arrow_right, size: 40.0 ),
+        child: Column(
+          children: <Widget>[
+
+            DropdownButton<String>(
+              isExpanded: true,
+              value: dropdownValue,
+              onChanged: (String newValue) {
+                setState( () {
+
+                  dropdownValue = newValue;
+
+                });
+              },
+              items: <String>['One', 'Two', 'Tree', 'Four'].map<DropdownMenuItem<String>>( (String value) {
+
+                return DropdownMenuItem<String>( value: value, child: Text(value) );
+
+              }).toList(),
+            ),
+
+            SizedBox(height: 15.0),
+
+            selectForm( dropdownValue ),
+
+          ],
         )
-      )
+      ),
     );
 
   }
