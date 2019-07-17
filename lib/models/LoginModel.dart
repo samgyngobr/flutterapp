161415aadb9474295c0ffe13790acb90b97df9f2
samgyngobr <samuel0360@gmail.com>
2019-07-17 11:10:26 -0300

    1

--- lib/models/LoginModel.dart
+++ lib/models/LoginModel.dart
@@ -7,6 +7,21 @@ class LoginModel
     print( globals.url['login']);
 
     //Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
+
+    /*
+      var modal = new Stack(
+        children: [
+          new Opacity(
+            opacity: 0.3,
+            child: const ModalBarrier(dismissible: false, color: Colors.grey),
+          ),
+          new Center(
+            child: new CircularProgressIndicator(),
+          ),
+        ],
+      );
+     */
+
     print(email);
     print(pass);
   }
