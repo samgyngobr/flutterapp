028f1a1653c4e3bca978ce658c6b7c79fad92a66
Samuel J. Oliveira <52247571+samgyngobr@users.noreply.github.com>
2019-07-20 23:59:01 -0300

    Update LoginModel.dart

--- lib/models/LoginModel.dart
+++ lib/models/LoginModel.dart
@@ -3,32 +3,6 @@ import 'dart:async';
 
 class LoginModel
 {
-  /*
-  LoginModel( email, pass )
-  {
-    print( globals.url['login']);
-
-    //Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
-
-
-      var modal = new Stack(
-        children: [
-          new Opacity(
-            opacity: 0.3,
-            child: const ModalBarrier(dismissible: false, color: Colors.grey),
-          ),
-          new Center(
-            child: new CircularProgressIndicator(),
-          ),
-        ],
-      );
-
-    print(email);
-    print(pass);
-
-  }
-  */
-
   Future loga( email, pass ) async
   {
     print( globals.url['login'] );
