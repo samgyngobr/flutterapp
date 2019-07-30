dfb85ccbe5f273fbfe0e9c04b012b17c39cf8c7e
samgyngobr <samuel0360@gmail.com>
2019-07-30 14:02:47 -0300

    1

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -16,6 +16,39 @@ class Home extends StatefulWidget
 class _HomeState extends State<Home> {
 
 
+
+  final List<Map<String, dynamic>> _listViewData = [
+    {
+      "title"  : "Titulo A",
+      "content": "Conteudo A",
+      "status" : "Atendimento",
+      "date"   : "12/04/2017"
+    },
+    {
+      "title"  : "Titulo B",
+      "content": "Conteudo B",
+      "status" : "Atendimento",
+      "date"   : "12/04/2017"
+    },
+    {
+      "title"  : "Titulo C",
+      "content": "Conteudo C",
+      "status" : "Atendimento",
+      "date"   : "12/04/2017"
+    },
+    {
+      "title"  : "Titulo D",
+      "content": "Conteudo D",
+      "status" : "Atendimento",
+      "date"   : "12/04/2017"
+    },
+  ];
+
+
+
+
+
+
   final cardA = Card(
     shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(15.0) ),
     elevation: 3,
@@ -115,6 +148,22 @@ class _HomeState extends State<Home> {
               cardC,
               SizedBox(height: 15.0),
 
+              SizedBox(
+                // you may want to use an aspect ratio here for tablet support
+                height: 100.0,
+                child: PageView.builder(
+                  // store this controller in a State to save the carousel scroll position
+                  itemCount: _listViewData.length,
+                  controller: PageController( viewportFraction: 0.9 ),
+                  itemBuilder: (BuildContext context, int itemIndex) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 4.0),
+                      child: _card( _listViewData[itemIndex] ),
+                    );
+                  },
+                ),
+              )
+
             ]
           ),
         )
@@ -124,4 +173,54 @@ class _HomeState extends State<Home> {
 
     );
   }
+
+
+
+
+  Widget _card( Map<String, dynamic> item ) {
+    return Container(
+
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+
+      child: Row(
+        children: <Widget>[
+
+          Expanded(
+            child: Container(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    Text( item['title']  , style: TextStyle( color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold ) ),
+                    Text( item['content'], style: TextStyle( color: Colors.white, fontSize: 18 ) ),
+
+                  ],
+                )
+            ),
+          ),
+
+          Container(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: <Widget>[
+
+                  Text( item['date'], style: TextStyle( color: Colors.white, fontSize: 16 ) ),
+
+                ],
+              )
+          ),
+
+        ],
+      ),
+
+    );
+  }
+
+
+
+
 }
