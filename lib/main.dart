27fbd6badc1dbbc570ef458a91a5d31f0c7f5a37
samgyngobr <samuel0360@gmail.com>
2019-08-01 15:46:52 -0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'pages/Home.dart';
+//import 'pages/Home.dart';
 import 'pages/SplashScreen.dart';
 
 
@@ -14,12 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterApp',
       theme: ThemeData( primarySwatch: Colors.blue ),
-      //home: Home( title: 'FlutterApp Login' ),
+      home : SplashScreen(),
+      /*
       routes: <String, WidgetBuilder>{
         'Home'         : ( BuildContext context ) => Home( title: 'FlutterApp Login' ),
         'SplashScreen' : ( BuildContext context ) => SplashScreen(),
       },
       initialRoute: 'SplashScreen',
+      */
     );
   }
 
--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -22,7 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   startTime() async {
     var _duration = new Duration(seconds: 3);
-    return new Timer(_duration, navigationPage);
+    return Timer(_duration, navigationPage);
   }
 
   void navigationPage() {
